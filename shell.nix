8d1bb681b573feed239bc63dd0caa87ba8f3354d
Sindri Magnússon <sim42@hi.is>
2026-02-03 10:36:52 +0100

    Demo app for time tracking

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# shell.nix
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d74de548348c46cf25cb1fcc4b74f38103a4590d.tar.gz") {};
+in pkgs.mkShell {
+  packages = [ 
+    pkgs.python314
+  ];
+  shellHook="
+  python -m venv venv
+  source venv/bin/activate
+  pip3 install Flask Flask-SQLAlchemy
+  python app.py
+  echo 'done'";
+}
