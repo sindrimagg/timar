bcb31b661f96cebc0adc66d6bf527c3b4e138a61
Sindri Magnússon <sim42@hi.is>
2026-02-03 11:23:41 +0100

    Made app.py the entry point

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ from flask import Flask
 from extensions import db
 from routes import clients_bp, projects_bp, hours_bp
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///time_tracker.db'
-    app.config['SECRET_KEY'] = 'dev-secret'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///time_tracker.db'
+app.config['SECRET_KEY'] = 'dev-secret'
 
-    db.init_app(app)
+db.init_app(app)
 
-    app.register_blueprint(clients_bp)
-    app.register_blueprint(projects_bp)
-    app.register_blueprint(hours_bp)
+app.register_blueprint(clients_bp)
+app.register_blueprint(projects_bp)
+app.register_blueprint(hours_bp)
 
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+app.run(debug=True)
