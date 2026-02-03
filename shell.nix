a108bd71fc51a066ccd467ef7a5377fed4a3f571
Sindri Magnússon <sim42@hi.is>
2026-02-03 12:39:10 +0100

    Kom þessu á íslensku og lagaði shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d74de548348c46cf25cb1fcc4b74f38103a4590d.tar.gz") {};
 in pkgs.mkShell {
   packages = [ 
-    pkgs.python314
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      pip
+    ]))
   ];
   shellHook="
   python -m venv venv
